b364c89bfc07c204b60bc5a1a22595417c9b0849
naten <natenathen.ar@gmail.com>
2026-01-30 19:26:14 +0600

    app updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,20 @@
-
-import './App.css'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import { Route } from 'react-router-dom'
-import { Routes } from 'react-router-dom'
-import Home from './pages/customer/Home'
-import Profile from './pages/customer/Profile'
-
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/customer/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Profile from "./pages/customer/Profile";
+import Cart from "./pages/customer/Cart";
 
 function App() {
-  
-
   return (
-    <>
-      <Routes>
-        
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/profile" element={<Profile/>} />
-        
-      </Routes>
-    </>
-    
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
